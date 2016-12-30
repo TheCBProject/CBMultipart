f4c23354d5cd90417953f770b49d7e59cd48f289
covers1624 <laughlan.cov@internode.on.net>
2016-12-31 05:29:05 +1030

    Fingerprint stuff, updte mcp mappings.

--- src/codechicken/multipart/handler/MultipartMod.scala
+++ src/codechicken/multipart/handler/MultipartMod.scala
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event._
 
-@Mod(modid = MultipartMod.modID, acceptedMinecraftVersions = "[1.10]", modLanguage = "scala")
+@Mod(modid = MultipartMod.modID, acceptedMinecraftVersions = "[1.10]", modLanguage = "scala", certificateFingerprint = "f1850c39b2516232a2108a7bd84d1cb5df93b261")
 object MultipartMod
 {
     final val modID = "forgemultipartcbe"
