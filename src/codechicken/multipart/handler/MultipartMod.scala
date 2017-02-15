69208bba8c213d0cd1571371900c9ebdb3cc611e
covers1624 <laughlan.cov@internode.on.net>
2017-02-16 09:40:41 +1030

    Bump CCL version

--- src/codechicken/multipart/handler/MultipartMod.scala
+++ src/codechicken/multipart/handler/MultipartMod.scala
@@ -1,14 +1,16 @@
 package codechicken.multipart.handler
 
+import codechicken.lib.CodeChickenLib
 import codechicken.multipart.MultiPartRegistry
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event._
 
-@Mod(modid = MultipartMod.modID, acceptedMinecraftVersions = "[1.10]", modLanguage = "scala", certificateFingerprint = "f1850c39b2516232a2108a7bd84d1cb5df93b261")
+@Mod(modid = MultipartMod.modID, dependencies = MultipartMod.deps, acceptedMinecraftVersions = "[1.10]", modLanguage = "scala", certificateFingerprint = "f1850c39b2516232a2108a7bd84d1cb5df93b261")
 object MultipartMod
 {
     final val modID = "forgemultipartcbe"
+    final val deps = "required-after:CodeChickenLib@[" + CodeChickenLib.version + ",)"
 
     @EventHandler
     def preInit(event:FMLPreInitializationEvent) {
