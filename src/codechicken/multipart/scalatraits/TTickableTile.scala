575e5a2195c0db5edceb1be6c073335c976786a8
covers1624 <laughlan.cov@internode.on.net>
2017-11-24 01:44:26 +1030

    Fix Ticking mixin not properly migrating state on replace.

--- src/codechicken/multipart/scalatraits/TTickableTile.scala
+++ src/codechicken/multipart/scalatraits/TTickableTile.scala
@@ -11,63 +11,76 @@ import scala.collection.JavaConversions._
   * Mixin Trait for parts implementing ITickable. Allows parts
   * to receive update calls every tick.
   */
-trait TTickableTile extends TileMultipart with ITickable
-{
+trait TTickableTile extends TileMultipart with ITickable {
+
+    private var doesTick = false
     var tickingParts = new JLinkedList[TMultiPart with ITickable]() //cache to reduce iteration of all parts
 
-    override def copyFrom(that:TileMultipart)
-    {
+    override def copyFrom(that: TileMultipart) {
         super.copyFrom(that)
         that match {
-            case tile:TTickableTile => tickingParts = tile.tickingParts
+            case tile: TTickableTile => {
+                tickingParts = tile.tickingParts
+                doesTick = tile.doesTick
+            }
             case _ =>
         }
     }
 
-    override def bindPart(part:TMultiPart)
-    {
+    override def bindPart(part: TMultiPart) {
         super.bindPart(part)
         part match {
-            case tickingPart:ITickable =>
+            case tickingPart: ITickable =>
                 tickingParts += tickingPart
                 setTicking(true)
             case _ =>
         }
     }
 
-    override def partRemoved(part:TMultiPart, p:Int)
-    {
+    override def partRemoved(part: TMultiPart, p: Int) {
         super.partRemoved(part, p)
         part match {
-            case tickingPart:ITickable => tickingParts -= tickingPart
-                //No need to disable ticking if empty, this tile will be replaced w/ nonticking one anyway
+            case tickingPart: ITickable => tickingParts -= tickingPart
+            //No need to disable ticking if empty, this tile will be replaced w/ nonticking one anyway
             case _ =>
         }
     }
 
-    override def clearParts()
-    {
+    override def clearParts() {
         super.clearParts()
         tickingParts.clear()
     }
 
-    override def update()
-    {
+    override def update() {
         val it = tickingParts.iterator
-        while(it.hasNext) {
+        while (it.hasNext) {
             val p = it.next()
-            if(p.tile != null) p.update()
+            if (p.tile != null) p.update()
         }
     }
 
-    private var doesTick = false
-    private def setTicking(tick:Boolean)
-    {
+    override def loadFrom(that: TileMultipart) {
+        super.loadFrom(that)
+        //Add the new tile to the ticking list.
+        if (doesTick) {
+            getWorld.tickableTileEntities.add(this)
+        }
+    }
+
+    override def loadTo(that: TileMultipart) {
+        super.loadTo(that)
+        //Remove the old tile from the ticking list.
+        if (doesTick) {
+            getWorld.tickableTileEntities.remove(this)
+        }
+    }
+
+    private def setTicking(tick: Boolean) {
         if (doesTick == tick) return
         doesTick = tick
 
-        if(getWorld != null && getWorld.getTileEntity(getPos) == this) {
-            if(tick)
+        if (getWorld != null && getWorld.getTileEntity(getPos) == this) {
+            if (tick)
                 getWorld.addTileEntity(this)
             else
                 getWorld.tickableTileEntities.remove(this)
@@ -78,6 +91,5 @@ trait TTickableTile extends TileMultipart with ITickable
 /**
   * To handle obfuscation issues, this is registered as a java trait.
   */
-class JTickableTile extends TileMultipart with TTickableTile
-{
+class JTickableTile extends TileMultipart with TTickableTile {
 }
