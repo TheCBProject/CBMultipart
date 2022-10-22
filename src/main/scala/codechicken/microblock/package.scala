1cbb7a1bc4960f0eafe5cb52d3da49f5ebd591ec
covers1624 <laughlan.cov@internode.on.net>
2022-10-23 00:02:56 +1030

    Purge the blight! Remove all scala!

--- src/main/scala/codechicken/microblock/package.scala
+++
@@ -1,8 +0,0 @@
-package codechicken
-
-import codechicken.microblock.utils.McImplicits
-import org.apache.logging.log4j.LogManager
-
-package object microblock extends McImplicits {
-    def logger = LogManager.getLogger("CBMicroblock")
-}
