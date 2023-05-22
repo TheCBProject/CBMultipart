9bd5b0f48a5e1193a1a959c4c069d16455ccd22f
MrTJP <mrtjp@icloud.com>
2023-05-21 21:12:54 -0400

    add missing button and torch types

---
+++ src/main/java/codechicken/multipart/minecraft/SoulTorchPart.java
@@ -0,0 +1,31 @@
+package codechicken.multipart.minecraft;
+
+import codechicken.multipart.api.MultipartType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class SoulTorchPart extends TorchPart {
+
+    public SoulTorchPart() {
+    }
+
+    public SoulTorchPart(BlockState state) {
+        super(state);
+    }
+
+    @Override
+    protected Block getStandingBlock() {
+        return Blocks.SOUL_TORCH;
+    }
+
+    @Override
+    protected Block getWallBlock() {
+        return Blocks.SOUL_WALL_TORCH;
+    }
+
+    @Override
+    public MultipartType<?> getType() {
+        return ModContent.soulTorchPartType;
+    }
+}
